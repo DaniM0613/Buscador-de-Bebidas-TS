8017528b68937c8c91b540a5c5d9ec3d6c29b8ef
DaniM0613 <wwh.rj.92@gmail.com>
2024-06-04 16:28:59 -0600

    creando el HEADER

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,7 +1,10 @@
+import Header from "../components/Header";
 
 export default function IndexPage () {
     return (
-        <h2>IndexPage</h2>
+        <>
+          <Header/>
+        </>
     )
 
 }
