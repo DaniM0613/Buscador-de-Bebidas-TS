b0ce3588752ee3b0ccf2f95065c0778a4796e9d5
DaniM0613 <wwh.rj.92@gmail.com>
2024-06-05 22:35:35 -0600

    mostrando las categorias en el formularion con map

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
      const isHome = useMemo(() => pathname === '/' , [pathname])
 
      const fetchCategories =  useAppStore((state) => state.fetchCategories)
-    
+     const categories = useAppStore((state) => state.categories)
 
      useEffect(() => {
          fetchCategories()
@@ -66,6 +66,14 @@ export default function Header() {
                               className='p-3 w-full rounded-lg '
                             >
                                 <option value=''>-- Seleccione --</option>
+                                {categories.drinks.map(category => (
+                                   <option
+                                     value={category.strCategory}
+                                     key={category.strCategory}
+                                   >
+                                    {category.strCategory}
+                                   </option>
+                                ))}
                             </select>
                           </div>
                           <input
