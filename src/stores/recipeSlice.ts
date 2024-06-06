89700dddd495543b911fbe842a10f5351b10ab54
DaniM0613 <wwh.rj.92@gmail.com>
2024-06-05 18:45:46 -0600

    obteniendo las categorias de la API y validar con ZOD

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,16 +1,20 @@
 import type { StateCreator } from "zustand"
 import { getCategories } from "../services/RecipeService"
-
-type Category = {}
+import type { Categories } from "../types"
 
 export type RecipeSliceType = {
-   categories: Category[]
+   categories: Categories
    fetchCategories: () => Promise<void>
 }
 
-export const createRecipeSlice : StateCreator<RecipeSliceType> = () => ({
-    categories: [],
+export const createRecipeSlice : StateCreator<RecipeSliceType> = (set) => ({
+    categories: {
+        drinks: []
+    },
     fetchCategories: async () => {
-         getCategories()
+         const categories = await getCategories()
+         set({
+            categories
+         })
     }
 })
