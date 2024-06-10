2483d394c70bc8d16503dd599142f6334fddf781
DaniM0613 <wwh.rj.92@gmail.com>
2024-06-10 15:41:58 -0600

    Seleccionar una receta para ver los detalles

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,12 +1,13 @@
 import type { StateCreator } from "zustand"
 import { getCategories, getRecipes } from "../services/RecipeService"
-import type { Categories, SearchFilter, Drinks } from "../types"
+import type { Categories, SearchFilter, Drinks, Drink } from "../types"
 
 export type RecipeSliceType = {
    categories: Categories
    drinks: Drinks
    fetchCategories: () => Promise<void>
    searchRecipes: (searchFilters: SearchFilter) => Promise<void>
+   selectRecipe: (id: Drink['idDrink']) => Promise<void>
 }
 
 export const createRecipeSlice : StateCreator<RecipeSliceType> = (set) => ({
@@ -27,5 +28,8 @@ export const createRecipeSlice : StateCreator<RecipeSliceType> = (set) => ({
           set({
             drinks
           })
+    },
+    selectRecipe : async (id) => {
+        console.log(id)
     }
 })
