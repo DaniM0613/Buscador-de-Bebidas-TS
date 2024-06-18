58ff9c3c1e9f8307e0940545c9da6c2b88e989bb
DaniM0613 <wwh.rj.92@gmail.com>
2024-06-17 22:11:30 -0600

    mostrar notificacion de todos los campos son obligatorios y perfomance en las apps con react router DOM y build

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,6 +14,7 @@ export default function Header() {
      const fetchCategories =  useAppStore((state) => state.fetchCategories)
      const categories = useAppStore((state) => state.categories)
      const searchRecipes = useAppStore((state) => state.searchRecipes)
+     const showNotification = useAppStore((state) => state.showNotification)
 
      useEffect(() => {
          fetchCategories()
@@ -31,8 +32,11 @@ export default function Header() {
 
       // Validar
       if(Object.values(searchFilters).includes('')){
-         console.log('Todos los campos son obligatorios')
-         return
+        showNotification({
+          text: 'Todos los campos son obligatorios',
+          error: true
+        })
+        return
       }
 
       // Consultar las receptas
